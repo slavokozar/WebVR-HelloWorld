7b6b90f38fdde9235cc4acb2db5e91f05716693c
Slavomir Kozar <slavo.kozar@gmail.com>
2018-10-20 17:52:00 +0200

    objektovy pacman

--- index.js
+++ index.js
@@ -1,38 +1,89 @@
+class Vector2 {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Pacman {
+    constructor(position) {
+        this.position = position;
+        this.diameter = 50;
+        this.angle = 38;
+    }
+
+    render(context) {
+        context.beginPath();
+
+        context.moveTo(
+            this.position.x,
+            this.position.y
+        );
+
+        context.lineTo(
+            this.position.x + Math.cos(this.angle * Math.PI / 180) * this.diameter,
+            this.position.y + Math.sin(this.angle * Math.PI / 180) * this.diameter
+        );
+
+        context.moveTo(
+            this.position.x,
+            this.position.y
+        );
+
+        context.lineTo(
+            this.position.x + Math.cos(-1 * this.angle * Math.PI / 180) * this.diameter,
+            this.position.y + Math.sin(-1 * this.angle * Math.PI / 180) * this.diameter
+        );
+
+        context.arc(
+            this.position.x,
+            this.position.y,
+            this.diameter,
+            (360 - this.angle) * Math.PI / 180,
+            this.angle * Math.PI / 180,
+            true
+        );
+
+        context.fill();
+    }
+
+    update(delta) {
+        this.position.x += delta.x;
+        this.position.y += delta.y;
+    }
+}
+
+
 const scene = document.getElementById('scene');
 
 const ctx = scene.getContext('2d');
 
+const pacman = new Pacman(
+    new Vector2(250, 250)
+);
 
 
-function loop(time){
+let timeLast = null;
 
-    console.log(time);
-    ctx.clearRect(0,0,scene.width, scene.height);
-    ctx.beginPath();
-
-    ctx.arc(
-        200,
-        200 + time / 100,
-        50,
-        320 * Math.PI / 180,
-        40 * Math.PI / 180,
-        true
-    );
-
-    ctx.moveTo(200, 200 + time / 100);
+pacman.render(ctx);
 
 
-    // for(let i=0;i<500;i++){
-    //     (i===0?ctx.moveTo:ctx.lineTo).call(ctx,i+time/100,Math.sin(i/10)*100+250);
-    // }
+// function loop(time) {
+//     ctx.clearRect(0, 0, scene.width, scene.height);
+//
+//     if (timeLast)
+//         pacman.update(new Vector2(0, (timeLast - time) / 100));
+//
+//     timeLast = time;
+//     pacman.render(ctx);
+//
+//
+//     requestAnimationFrame(loop);
+// }
+//
+//
+//
+//
+// requestAnimationFrame(loop);
 
 
-    ctx.stroke();
-    // ctx.fill();
-
-    requestAnimationFrame(loop);
-}
-
-
-
-requestAnimationFrame(loop);
